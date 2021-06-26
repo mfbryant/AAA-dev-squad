99150c45ec05fb05880aa6cebf22d83cbda28142
Mattie Bryant <mfbryant@crimson.ua.edu>
2021-06-26 11:12:02 -0500

    new folders

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("app be working");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>THANK YOU LORD</Text>
       <StatusBar style="auto" />
     </View>
   );

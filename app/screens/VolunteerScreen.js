12ecd6b7c9fc7bc7369638ebe8de9f8b8a8b5576
Ben Reeves <bireeves@crimson.ua.edu>
2021-07-20 14:09:15 -0500

    Volunteer Screen Improvements

--- app/screens/VolunteerScreen.js
+++ app/screens/VolunteerScreen.js
@@ -6,27 +6,30 @@ import { StatusBar } from 'expo-status-bar';
 import Screen from '../assets/components/Screen';
 import Icon from '../assets/components/IconButton';
 import Post from '../assets/components/Post';
-import routes from '../navigation/route';
 
 const fakePosts = [
     {
         id: 1,
+        group: 'CMISS',
         title: "Involvement oppurtunity at soup bowl!",
         subtitle: 'Going to make this a very long text to make sure we are still handling everything the way we should be handling it'
     },
 
     {
         id: 2,
+        group: 'AIMS',
         title: "Involvement oppurtunity",
         subtitle: "Get involved today! There needs everywhere!"
     },
     {
         id: 3,
+        group: 'WIT',
         title: "Get Involved!",
         subtitle: "Today is the day to do this and that and allll that"
     },
     {
         id: 4,
+        group: "AIMS AMBASSADOR",
         title: "SIGN UP NOW!",
         subtitle: "Tomorrow"
     }
@@ -36,6 +39,7 @@ const fakePosts = [
 function VolunteerScreen({ navigation }) {
     
     const [posts, setPost] = useState(fakePosts);
+    const [refreshing, setRefreshing] = useState(false);
     //Possible Icons: close, arrow-left, chevron-left/chevron-right/chevron-down/chevron-up
     return (
     <Screen>
@@ -61,11 +65,16 @@ function VolunteerScreen({ navigation }) {
                 <FlatList 
                 data={posts}
                 keyExtractor={post => post.id.toString()} 
-                renderItem={({ item }) => <Post 
+                renderItem={({ item }) => <Post
+                group={item.group} 
                 title={item.title}
                 subtitle={item.subtitle}
-                onPress={() => navigation.navigate(routes.VOLUNTEER_DETAILS , item)}
-                />}/>
+                onPress={() => navigation.navigate('VolunteerDetail' , item)}
+                />}
+                refreshing={refreshing}
+                onRefresh={() => {setPost(fakePosts);
+                }}
+                />
             </View>
           </ImageBackground>
         </Screen>

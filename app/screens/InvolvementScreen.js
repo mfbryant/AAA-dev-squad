fd0b1bd251317378fa57b8850473ed52c7549ded
Ben Reeves <bireeves@crimson.ua.edu>
2021-08-23 14:45:28 -0500

    involvement

--- app/screens/InvolvementScreen.js
+++ app/screens/InvolvementScreen.js
@@ -5,6 +5,7 @@ import { AppButton } from "../assets/components/Button";
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import SponsorsScreen from './SponsorsScreen';
 import FeedNavigator from '../navigation/FeedNavigator';
 
@@ -22,6 +23,11 @@ function InvolvementScreen({ navigation }) {
 import SponsorsScreen from "./SponsorsScreen";
 
 function InvolvementScreen({ navigation }) {
+=======
+import SponsorsScreen from "./SponsorsScreen";
+
+function InvolvementScreen({ navigation }) {
+>>>>>>> a2938060f0f4184335f8a978522ff6e94ae281f8
 =======
 import SponsorsScreen from "./SponsorsScreen";
 
@@ -52,6 +58,9 @@ function InvolvementScreen({ navigation }) {
   );
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> a2938060f0f4184335f8a978522ff6e94ae281f8
+=======
 >>>>>>> a2938060f0f4184335f8a978522ff6e94ae281f8
 =======
 >>>>>>> a2938060f0f4184335f8a978522ff6e94ae281f8

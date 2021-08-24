db4e0ea2fe737e43131af4313d60bca779c26ed8
Joseph May <jdmay2@crimson.ua.edu>
2021-08-24 15:33:38 -0500

    Fix

--- app/screens/VolunteerScreen.js
+++ app/screens/VolunteerScreen.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { StyleSheet, View, ImageBackground, FlatList, TouchableOpacity, Text } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
-
-import Screen from '../assets/components/Screen';
-import Icon from '../assets/components/IconButton';
-import Post from '../assets/components/Post';
-import colors from '../assets/config/colors';
-
-
-const fakePosts = [
-    {
-        id: 1,
-        group: 'CMISS',
-        title: "Involvement oppurtunity at soup bowl!",
-        subtitle: 'Going to make this a very long text to make sure we are still handling everything the way we should be handling it',
-        time: '2 minutes ago'
-    },
-
-    {
-        id: 2,
-        group: 'AIMS',
-        title: "Involvement oppurtunity",
-        subtitle: "Get involved today! There needs everywhere!",
-        time: '3 days ago',
-    },
-    {
-        id: 3,
-        group: 'WIT',
-        title: "Get Involved!",
-        subtitle: "Today is the day to do this and that and allll that",
-        time: '35 minutes ago'
-    },
-    {
-        id: 4,
-        group: "AMBASSADOR",
-        title: "SIGN UP NOW!",
-        subtitle: "Tomorrow",
-        time: '2 hours ago'
-    }
-
-]
-
-function VolunteerScreen({ navigation }) {
-    
-    const [posts, setPost] = useState(fakePosts);
-    const [refreshing, setRefreshing] = useState(false);
-    //Possible Icons: close, arrow-left, chevron-left/chevron-right/chevron-down/chevron-up
-    return (
-    // <Screen>
-        <ImageBackground style={styles.image} backgroundColor="#e9e2e4">
-            <StatusBar style="light"/>
-            <View style={{flexDirection: 'row', paddingTop: 45, backgroundColor: colors.charcoal, justifyContent: 'center'}}>
-            <Icon
-=======
 import React, { useState } from "react";
 import {
   StyleSheet,
@@ -62,6 +5,7 @@ import {
   ImageBackground,
   FlatList,
   TouchableOpacity,
+  Text,
 } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
@@ -69,6 +13,7 @@ import { StatusBar } from "expo-status-bar";
 import Screen from "../assets/components/Screen";
 import { IconButton } from "../assets/components/Button";
 import Post from "../assets/components/Post";
+import colors from "../assets/config/colors";
 
 const fakePosts = [
   {
@@ -77,6 +22,7 @@ const fakePosts = [
     title: "Involvement oppurtunity at soup bowl!",
     subtitle:
       "Going to make this a very long text to make sure we are still handling everything the way we should be handling it",
+    time: "2 minutes ago",
   },
 
   {
@@ -84,18 +30,21 @@ const fakePosts = [
     group: "AIMS",
     title: "Involvement oppurtunity",
     subtitle: "Get involved today! There needs everywhere!",
+    time: "3 days ago",
   },
   {
     id: 3,
     group: "WIT",
     title: "Get Involved!",
     subtitle: "Today is the day to do this and that and allll that",
+    time: "35 minutes ago",
   },
   {
     id: 4,
-    group: "AIMS AMBASSADOR",
+    group: "AMBASSADOR",
     title: "SIGN UP NOW!",
     subtitle: "Tomorrow",
+    time: "2 hours ago",
   },
 ];
 
@@ -104,113 +53,61 @@ function VolunteerScreen({ navigation }) {
   const [refreshing, setRefreshing] = useState(false);
   //Possible Icons: close, arrow-left, chevron-left/chevron-right/chevron-down/chevron-up
   return (
-    <Screen>
-      <ImageBackground style={styles.image} backgroundColor="#e9e2e4">
-        <StatusBar style="auto" />
-        {/* <View style={{flexDirection: 'row'}}>
-            <IconButton
->>>>>>> a2938060f0f4184335f8a978522ff6e94ae281f8
-            style={styles.icon}
-            name="close" 
-            color='white' 
-            onPress={() => console.log("exit pressed")} 
-<<<<<<< HEAD
-            size={25}/> 
-            <Text style={styles.text}>Volunteer Oppurtunities</Text>
-            <Icon name='plus' 
-                    onPress={() => 
-                    console.log('Add Pressed')} 
-                    size={25} 
-                    color='white'
-                    style={styles.add}
-                />
-            </View>
-            <View style={styles.container}>
-                <FlatList 
-                data={posts}
-                keyExtractor={post => post.id.toString()} 
-                renderItem={({ item }) => <Post
-                group={item.group} 
-                title={item.title}
-                subtitle={item.subtitle}
-                time={item.time}
-                onPress={() => navigation.navigate('VolunteerDetail' , item)}
-                />}
-                refreshing={refreshing}
-                onRefresh={() => {setPost(fakePosts);
-                }}
-                />
-            </View>
-          </ImageBackground>
-        // </Screen>
-    );
-}
-
-const styles = StyleSheet.create({
-    add: {
-        paddingTop: 7,
-        margin: 5,
-        },
-    container: {
-        flex: 1,
-        width: '100%',
-    },
-    icon: {
-        paddingRight: 10,
-        paddingTop: 7,
-        paddingBottom: 4,
-        margin: 5
-    },
-    image: {
-        flex: 1,
-    },
-    text: {
-        color: 'white', 
-        alignSelf: 'center',
-        marginLeft: 35, 
-        fontSize: 18, 
-        marginRight: 35,
-        fontWeight: 'bold' 
-    },
-})
-=======
-            size={40}/>
-            <TouchableOpacity>
-                    <IconButton name='plus-box' 
-                    onPress={() => 
-                    console.log('Add Pressed')} 
-                    size={40} 
-                    color='crimson'
-                    style={styles.add}/>
-                </TouchableOpacity>
-            </View> */}
-        <View style={styles.container}>
-          <FlatList
-            data={posts}
-            keyExtractor={(post) => post.id.toString()}
-            renderItem={({ item }) => (
-              <Post
-                group={item.group}
-                title={item.title}
-                subtitle={item.subtitle}
-                onPress={() => navigation.navigate("VolunteerDetail", item)}
-              />
-            )}
-            refreshing={refreshing}
-            onRefresh={() => {
-              setPost(fakePosts);
-            }}
-          />
-        </View>
-      </ImageBackground>
-    </Screen>
+    // <Screen>
+    <ImageBackground style={styles.image} backgroundColor="#e9e2e4">
+      <StatusBar style="light" />
+      <View
+        style={{
+          flexDirection: "row",
+          paddingTop: 45,
+          backgroundColor: colors.charcoal,
+          justifyContent: "center",
+        }}
+      >
+        <IconButton
+          style={styles.icon}
+          name="close"
+          color="white"
+          onPress={() => console.log("exit pressed")}
+          size={25}
+        />
+        <Text style={styles.text}>Volunteer Oppurtunities</Text>
+        <IconButton
+          name="plus"
+          onPress={() => console.log("Add Pressed")}
+          size={25}
+          color="white"
+          style={styles.add}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={posts}
+          keyExtractor={(post) => post.id.toString()}
+          renderItem={({ item }) => (
+            <Post
+              group={item.group}
+              title={item.title}
+              subtitle={item.subtitle}
+              time={item.time}
+              onPress={() => navigation.navigate("VolunteerDetail", item)}
+            />
+          )}
+          refreshing={refreshing}
+          onRefresh={() => {
+            setPost(fakePosts);
+          }}
+        />
+      </View>
+    </ImageBackground>
+    // </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   add: {
-    paddingLeft: 310,
-    paddingTop: 10,
+    paddingTop: 7,
+    margin: 5,
   },
   container: {
     flex: 1,
@@ -218,12 +115,21 @@ const styles = StyleSheet.create({
   },
   icon: {
     paddingRight: 10,
-    paddingTop: 10,
+    paddingTop: 7,
+    paddingBottom: 4,
+    margin: 5,
   },
   image: {
     flex: 1,
   },
+  text: {
+    color: "white",
+    alignSelf: "center",
+    marginLeft: 35,
+    fontSize: 18,
+    marginRight: 35,
+    fontWeight: "bold",
+  },
 });
->>>>>>> a2938060f0f4184335f8a978522ff6e94ae281f8
 
 export default VolunteerScreen;
